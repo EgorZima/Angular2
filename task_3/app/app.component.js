7ab40bb2d6423ef2a5a76c861d26a0f68b1fd4bf
EgorZima <zimaegor21@gmail.com>
2017-04-12 22:44:36 +0300

    Improved task 3, added new features and binding

--- task_3/app/app.component.js
+++ task_3/app/app.component.js
@@ -8,26 +8,39 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var Products = [
-    { id: 1, name: 'product 1', price: 100 },
-    { id: 2, name: 'product 2', price: 200 },
-    { id: 3, name: 'product 3', price: 300 },
-    { id: 4, name: 'product 4', price: 400 },
-    { id: 5, name: 'product 5', price: 500 },
-    { id: 6, name: 'product 6', price: 600 },
-    { id: 7, name: 'product 7', price: 700 },
-    { id: 8, name: 'product 8', price: 800 },
-    { id: 9, name: 'product 9', price: 900 },
-    { id: 10, name: 'product 10', price: 1000 }
+    { id: 1, name: 'product 1', price: 100, category: 1 },
+    { id: 2, name: 'product 2', price: 200, category: 2 },
+    { id: 3, name: 'product 3', price: 300, category: 3 },
+    { id: 4, name: 'product 4', price: 400, category: 1 },
+    { id: 5, name: 'product 5', price: 500, category: 2 },
+    { id: 6, name: 'product 6', price: 600, category: 3 },
+    { id: 7, name: 'product 7', price: 700, category: 1 },
+    { id: 8, name: 'product 8', price: 800, category: 2 },
+    { id: 9, name: 'product 9', price: 900, category: 3 },
+    { id: 10, name: 'product 10', price: 1000, category: 1 }
 ];
 var AppComponent = (function () {
     function AppComponent() {
         this.products = Products;
+        this.value = 500;
+        this.settings = {
+            red: 'red',
+            price: 'darkgoldenrod'
+        };
     }
     AppComponent.prototype.delete = function (item) {
         var index = this.products.indexOf(item);
         if (index > -1) {
             this.products.splice(index, 1);
         }
+    };
+    AppComponent.prototype.add = function () {
+        this.products.push({
+            id: this.products.length + 1,
+            name: this.title,
+            price: this.price,
+            category: this.category
+        });
     };
     return AppComponent;
 }());
--- task_3/app/app.component.ts
+++ task_3/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from "@angular/core";
 
 let Products = [
-    { id: 1, name : 'product 1', price : 100 },
-    { id: 2, name : 'product 2', price : 200 },
-    { id: 3, name : 'product 3', price : 300 },
-    { id: 4, name : 'product 4', price : 400 },
-    { id: 5, name : 'product 5', price : 500 },
-    { id: 6, name : 'product 6', price : 600 },
-    { id: 7, name : 'product 7', price : 700 },
-    { id: 8, name : 'product 8', price : 800 },
-    { id: 9, name : 'product 9', price : 900 },
-    { id: 10, name: 'product 10', price : 1000 }
+    { id: 1, name : 'product 1', price : 100, category: 1 },
+    { id: 2, name : 'product 2', price : 200, category: 2 },
+    { id: 3, name : 'product 3', price : 300, category: 3 },
+    { id: 4, name : 'product 4', price : 400, category: 1 },
+    { id: 5, name : 'product 5', price : 500, category: 2 },
+    { id: 6, name : 'product 6', price : 600, category: 3 },
+    { id: 7, name : 'product 7', price : 700, category: 1 },
+    { id: 8, name : 'product 8', price : 800, category: 2 },
+    { id: 9, name : 'product 9', price : 900, category: 3 },
+    { id: 10, name: 'product 10', price: 1000,category: 1 }
 ];
 
 
@@ -22,6 +22,16 @@ let Products = [
 }) 
 export class AppComponent {
     products = Products;
+    value = 500;
+    choice;
+    title;
+    price;
+    category;
+    
+    settings = {
+        red: 'red',
+        price: 'darkgoldenrod'
+    }
 
     delete(item) {
         let index = this.products.indexOf(item);
@@ -29,6 +39,17 @@ export class AppComponent {
         if (index > -1) {
              this.products.splice(index, 1);
         }     
+    }
+
+    add() {
+        this.products.push(
+            { 
+              id: this.products.length + 1, 
+              name: this.title, 
+              price: this.price,
+              category: this.category
+            }
+        )
     }
 
 }
